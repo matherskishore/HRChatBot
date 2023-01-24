79feac02d3814268509ab2519669ed49cb1f21f4
matherskishore <kishore.r@integrumsolutions.in>
2023-01-24 09:58:50 +0530

    changed server link

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server (bot's response)  
- const response = await fetch('https://krish-hr-helperbot-powered-by-openai.onrender.com', {
+ const response = await fetch('https://krish.onrender.com', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
